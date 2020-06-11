89314a7c9b8231728b7d2438aae8e647c18fd498
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-06-11 10:58:21 -0300

    Code improvement

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import requests
 def get_temperature(lat, lng):
     key = 'e1ee55658d4a2b28c4841e373c3b3d87'
     url = 'https://api.darksky.net/forecast/{}/{},{}'.format(key, lat, lng)
-    reponse = requests.get(url)
-    data = reponse.json()
-    temperature = data.get('currently').get('temperature')
-    if not temperature:
-        return
-    return int((temperature - 32) * 5.0 / 9.0)
+    try:
+        reponse = requests.get(url)
+        data = reponse.json()
+        temperature = data.get('currently').get('temperature')
+        return int((temperature - 32) * 5.0 / 9.0)
+    except AttributeError:
+        return data
+    except TypeError:
+        return "Invalid type data"
--- test_main.py
+++ test_main.py
@@ -2,32 +2,83 @@ from main import get_temperature
 from unittest.mock import patch
 import pytest
 
-temperature_lat_lng_expected = [
+
+######## VALID TESTS #########
+valid_tests = [
     (62, -14.235004, -51.92528, 16),
-    (32, 50.652544, 32.852458, 0),
-    (50, -10.856584, 25.548, 10)
+    (62.54, -14.235004, -51.92528, 16),
+    (5, 15.58564, -13.85478, -15),
+    (-13, 22.8523, 17.358746, -25),
+    (0, 22.8523, 17.358746, -17),
 ]
 
 
-@pytest.mark.parametrize("temperature,lat,lng,expected", temperature_lat_lng_expected)
-def test_get_temperature_by_lat_lng(temperature, lat, lng, expected):
+@pytest.mark.parametrize("rTemperature,lat,lng,expected", valid_tests)
+def test_get_temperature_by_lat_lng(rTemperature, lat, lng, expected):
 
     mock_get_patcher = patch('main.requests.get')
-
-    temperature = {
-        "currently": {
-            "temperature": temperature,
-        }
-    }
+    temperature = {"currently": {"temperature": rTemperature}}
 
     mock_get = mock_get_patcher.start()
-
     mock_get.return_value.json.return_value = temperature
+    response = get_temperature(lat, lng)
+    mock_get_patcher.stop()
+
+    assert response == expected
+
+
+######## TESTs WITH INVALID LAT/LNG #########
+invalid_lat_lng = [
+    (62, 90.003453, 17.358746, {"code": 400,
+                                "error": "The given location is invalid."}),
+    (62, -90.003453, 17.358746, {"code": 400,
+                                 "error": "The given location is invalid."}),
+    (62, 32.23456, 180.358746, {"code": 400,
+                                "error": "The given location is invalid."}),
+    (62, 32.23456, -180.358746, {"code": 400,
+                                 "error": "The given location is invalid."}),
+]
+
+
+@pytest.mark.parametrize("rTemperature,lat,lng,expected", invalid_lat_lng)
+def test_get_temperature_with_invalid_lat_lng(rTemperature, lat, lng, expected):
 
     response = get_temperature(lat, lng)
 
-    mock_get_patcher.stop()
+    assert response == expected
 
-    print(response)
+
+######## TESTs WITH RANDOM LAT/LNG TYPE #########
+invalid_lat_lng_type = [
+    (62, None, 17.358746, {'code': 400, 'error': 'Poorly formatted request'}),
+    (62, -90.003453, "Test", {'code': 400,
+                              'error': 'Poorly formatted request'}),
+]
+
+
+@pytest.mark.parametrize("rTemperature,lat,lng,expected", invalid_lat_lng_type)
+def test_get_temperature_with_invalid_lat_lng_type(rTemperature, lat, lng, expected):
+
+    response = get_temperature(lat, lng)
+
+    assert response == expected
+
+
+######## TESTs WITH UNEXPECTED RETURN #########
+unexpected_return = [
+    (None, 89.98345, 17.358746, "Invalid type data"),
+    ('Test', 15.23098, 17.358746, "Invalid type data"),
+]
+
+
+@pytest.mark.parametrize("rTemperature,lat,lng,expected", unexpected_return)
+def test_get_temperature_with_unexpected_return(rTemperature, lat, lng, expected):
+    mock_get_patcher = patch('main.requests.get')
+    temperature = {"currently": {"temperature": rTemperature}}
+
+    mock_get = mock_get_patcher.start()
+    mock_get.return_value.json.return_value = temperature
+    response = get_temperature(lat, lng)
+    mock_get_patcher.stop()
 
     assert response == expected
